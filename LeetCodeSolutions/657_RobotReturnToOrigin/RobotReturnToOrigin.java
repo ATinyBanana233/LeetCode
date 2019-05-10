596b5d93e9be1f86ae2763af359c72dcc7e2bd96
ATinyBanana <mich_li@hotmail.com>
2019-05-09 19:54:11 -0700

    Modifying standard and readme

--- LeetCodeSolutions/657_RobotReturnToOrigin/RobotReturnToOrigin.java
+++ LeetCodeSolutions/657_RobotReturnToOrigin/RobotReturnToOrigin.java
@@ -1,7 +1,12 @@
 package string;
 
 public class RobotReturnToOrigin {
-    
+    /**
+     * Function to determine a robot is back to the original by reading a string
+     * containing the moves. U - up, D - down, L - left, R - right.
+     * @param moves     String containing the moves
+     * @return          True if robot is back to the origin, false otherwise.
+     */
     public boolean judgeCircle(String moves) {
         int uNum = 0, dNum = 0, lNum = 0, rNum = 0;
         for (Character c : moves.toCharArray())
@@ -22,7 +27,6 @@ public class RobotReturnToOrigin {
                     break;
             }
         }
-
         return (uNum == dNum) && (lNum == rNum);
     }
 }
