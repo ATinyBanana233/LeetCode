474b377a465261ca438c1b7aa902dc97fa66b30b
ATinyBanana <mich_li@hotmail.com>
2019-05-12 18:01:22 -0700

    modify binary search

--- LeetCodeSolutions/509_FibonacciNumber/FibonacciNumber.java
+++ LeetCodeSolutions/509_FibonacciNumber/FibonacciNumber.java
@@ -12,45 +12,50 @@ public class Fibonacci {
 
     /**
      * Function to recursively calculate fibonacci number.
-     * @param n         The nth fibonacci number.
+     * @param N         The nth fibonacci number.
      * @return          The nth fibonacci number.
      */
-    public static int fibRecursive(int n) {
+    public static int fibRecursive(int N) {
         // Check
-        if (n < 0)
+        if (N < 0)
         {
             throw new IllegalArgumentException();
         }
-        else if (n <= 1)
+        else if (N <= 1)
         {
-            return n;
+            return N;
         }
 
-        return fibRecursive(n - 2) + fibRecursive( n - 1);
+        return fibRecursive(N - 2) + fibRecursive( N - 1);
     }
 
     /**
      * Function to iteratively calculate fibonacci number.
-     * @param n         The nth fibonacci number.
+     * @param N         The nth fibonacci number.
      * @return          The nth fibonacci number.
      */
-    public static int fibIterative(int n) {
+    public static int fibIterative(int N) {
         // Check
-        if (n < 0)
+        if (N < 0)
         {
             throw new IllegalArgumentException();
         }
 
-        // Fibonacci number start with 0th as well, so when n = 3, we have 4 fibonacci numbers.
+        if (N <= 1)
+        {
+            return N;
+        }
+
+        // Fibonacci number start with 0th as well, so when N = 3, we have 4 fibonacci numbers.
         // One more slot to hold 0th fibonacci number.
-        int fib[] = new int[n + 1] ;
+        int fib[] = new int[N + 1] ;
         fib[0] = 0;
         fib[1] = 1;
-        for (int i = 2; i <= n; i++)
+        for (int i = 2; i <= N; i++)
         {
             fib[i] = fib[i - 2] + fib[i - 1];
         }
 
-        return fib[n];
+        return fib[N];
     }
 }
