ea19bcce51d23319782c53cbc54f5e61c2eb4830
ATinyBanana <mich_li@hotmail.com>
2019-05-12 19:36:05 -0700

    add leetcode230 doc

--- LeetCodeSolutions/230_KthSmallestElementInBST/KthSmallestElementInBST.java
+++ LeetCodeSolutions/230_KthSmallestElementInBST/KthSmallestElementInBST.java
@@ -8,12 +8,23 @@
  * }
  */
 class Solution {
+    /**
+     * Find the kth smallest element in the BST.
+     * @param root          Root of the BST.
+     * @param k             kth element.
+     * @return              The value of the kth element.
+     */
     public int kthSmallest(TreeNode root, int k) {
         List<Integer> store = new ArrayList<>();
         helperFindKthSmallest(root, store);
         return store.get(k-1);
     }
 
+    /**
+     * Helper function to traverse the tree using inorder and store all the value in the given array list.
+     * @param root          Root of the BST to be traversed.
+     * @param store         The array list used for storing values.
+     */
     public void helperFindKthSmallest(TreeNode root, List<Integer> store) {
         if (root == null)
         {
