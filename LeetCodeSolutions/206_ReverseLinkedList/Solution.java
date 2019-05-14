ae14db55ac704ffc832a3903d689ff70adab4a1b
ATinyBanana <mich_li@hotmail.com>
2019-05-13 18:06:11 -0700

    add doc

--- LeetCodeSolutions/206_ReverseLinkedList/Solution.java
+++ LeetCodeSolutions/206_ReverseLinkedList/Solution.java
@@ -7,15 +7,26 @@
  * }
  */
 class Solution {
+    /**
+     * Function to reverse a linked list.
+     * @param head          Head pointer.
+     * @return              Head pointer
+     */
     public ListNode reverseList(ListNode head) {
         ListNode currToProcess = head, nextToProcess = null, prev = null;
+
         while (currToProcess != null) {
+            // Save the next node that needs to process.
             nextToProcess = currToProcess.next;
+            // Change current.next.
             currToProcess.next = prev;
+            // Prev would be current since we are doing reversed order.
             prev = currToProcess;
+            // Next to process will be the next current to process.
             currToProcess = nextToProcess;
         }
 
+        // Change the head.
         head = prev;
         return head;
     }
