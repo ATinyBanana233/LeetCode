80f9ccac03cfd47529fed98e100b4babebf3d5de
ATinyBanana <mich_li@hotmail.com>
2019-05-20 22:29:41 -0700

    Happy 520

--- LeetCodeSolutions/Other/Heart.sh
+++ LeetCodeSolutions/Other/Heart.sh
@@ -36,32 +36,27 @@ makeHeart()
 
   done
 
-  #outer loop is for printing number of rows in the pyramid
+  # Inverse pyramid
   for((i=n;i>=0;i--))
   do
-
-      #This loop print spaces required
       for((k=0;k<=n-i;k++))
       do
         echo -ne " ";
         sleep 0.001
       done
 
-      #This loop print part1 of the the pyramid
       for((j=1;j<=i-1;j++))
       do
       echo -ne "*";
       sleep 0.001
       done
 
-      #This loop print part 2 of the pryamid.
       for((z=1;z<i-1;z++))
       do
       echo -ne "*";
       sleep 0.001
       done
       
-      #This echo used for printing new line
       echo;
   done
 }
